c8437fe448fbebdb1049a44bcec1713222cc24f5
Rashad <rashad.hammonds0113@gmail.com>
2022-09-19 14:15:28 -0400

    first commit

---
+++ client/weatherman/src/componet/Backdrop.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="maincardcontainer">
+        <input type="text" placeholder='search' className='search-bar'/>
+        <button className='search-button'>Search</button>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ client/weatherman/src/componet/Maincard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Maincard extends Component {
+  render() {
+    return (
+      <div className='cardContainer'>
+        <div className='search'>\
+        <input type='text'className='seachbar' placeholder='enter location here'/>
+
+        </div>
+      </div>
+    )
+  }
+}
